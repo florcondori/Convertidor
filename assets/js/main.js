b4e3381b7e494a95330fa6a2beb816f8bae1c3b2
flor condori <condorihuamanif@gmail.com>
2017-03-03 19:32:50 -0500

    colocando parseFloat

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 function convertiraCelsius(){
-	var far = document.getElementById("farenheit").value;
+	var far = parseFloat(document.getElementById("farenheit").value);
 		if( far != ""){
 			document.getElementById("celsius").value = ((far-32)*5/9).toFixed(2);
 		}
@@ -8,7 +8,7 @@ function convertiraCelsius(){
 }
 
 function convertiraFarenheit(){
-	var cel = document.getElementById("celsius").value;
+	var cel = parseFloat(document.getElementById("celsius").value);
 		if(cel != ""){
 			document.getElementById("farenheit").value =((cel*9/5)+32).toFixed(2);
 		}
